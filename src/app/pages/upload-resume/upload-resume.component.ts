a7a88c40f5f90233bd15f5aaed959249a546f0d9
Ivan Moreno <Ivanmoreno_708@hotmail.com>
2023-05-16 14:40:34 +0200

    Validation_form_upload_component_and_navbar_underline

--- src/app/pages/upload-resume/upload-resume.component.ts
+++ src/app/pages/upload-resume/upload-resume.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-upload-resume',
@@ -6,6 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-resume.component.css']
 })
 export class UploadResumeComponent {
+
+  //Form validation
+
+  registerForm = new FormGroup({
+    fname: new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    lname: new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    phone: new FormControl('',[Validators.required]),
+    uploadResume: new FormControl('',[Validators.required,Validators.maxLength(50)])
+
+  })
+
+  onSubmit(){
+    console.warn(this.registerForm.value)
+  }
+
+  get name(){
+    return this.registerForm.get('name');
+  }
+
+  get surname(){
+    return this.registerForm.get('surname');
+  }
+
+  get email(){
+    return this.registerForm.get('email');
+  }
+
+  get comment(){
+    return this.registerForm.get('comment');
+  }
+
 
 
 }
