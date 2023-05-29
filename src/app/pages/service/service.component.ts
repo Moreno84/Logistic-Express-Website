456d335757d8f39df49f9f2ae1617221e60ec0c6
Ivan Moreno <Ivanmoreno_708@hotmail.com>
2023-05-29 12:01:40 +0200

    Adding_title_and_text_&_Modifying_images

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -7,9 +7,29 @@ import { Component } from '@angular/core';
 })
 export class ServiceComponent {
 
-  imagePlane:string="./assets/images/img_airplane_service_air.png";
-  imageMaritime:string="./assets/images/img_cruiseship_service_maritime.png";
-  imageBus:string="./assets/images/img_bus_service_road.png";
-  imageTrain:string="./assets/images/img_train_service_railway.png";
+
+services: any =[
+    {
+      'image': "./assets/images/img_airplane_service_air.png",
+      'title':"Our air",
+      'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas."
+    },
+    {
+      'image': "./assets/images/img_cruiseship_service_maritime.png",
+      'title':"Our maritime",
+      'text': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas."
+    },
+    {
+      'image': "./assets/images/img_train_service_railway.png",
+      'title':"Our road",
+      'text':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas."
+    },
+    {
+      'image': "./assets/images/img_bus_service_road.png",
+      'title':"Our railway",
+      'text':"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet aliquam id diam maecenas."  
+    }
+
+  ]
 
 }
