45a0118e2fcd577cc97a62a8bbd322c5c4b66b8e
Ivan Moreno <Ivanmoreno_708@hotmail.com>
2023-12-08 19:09:52 +0100

    Website

--- src/app/pages/get-in-touch/get-in-touch.component.ts
+++ src/app/pages/get-in-touch/get-in-touch.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
 import { contact } from 'src/app/services/contact';
-// import { Contact } from 'src/app/services/contact';
 import { FormGetInTouchService } from 'src/app/services/form-get-in-touch.service';
 
 @Component({
@@ -11,7 +10,8 @@ import { FormGetInTouchService } from 'src/app/services/form-get-in-touch.servic
   providers: [FormGetInTouchService]
 })
 export class GetInTouchComponent implements OnInit {
-[x: string]: any;
+
+
   // Image
     imageMap:string="./assets/images/map.png";
   // Title
@@ -20,12 +20,12 @@ export class GetInTouchComponent implements OnInit {
     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec metus massa.Suspendisse gravida ultricies velit eget volutpat. Integer vulputate est eu rutrum mattis. Quisque dictum ultricies nulla non consequat. Aliquam a neque sit amet libero congue congue in id enim. Nunc suscipit bibendum nisi. Nullam turpis nisi, porttitor non enim id, ornare auctor risus. Sed sed pretium nisi, vitae bibendum ipsum. Pellentesque a mauris augue.";
 
   //contact
-  contacts:any;
+contacts:any;
 
    constructor(private formGetInTouchService: FormGetInTouchService){
-    // this.formGetInTouchService.saveData(this.contacts).subscribe((data)=>{
-    //   this.contacts = data;
-    // })
+    this.formGetInTouchService.saveData(this.contacts).subscribe((data)=>{
+      this.contacts = data;
+    })
    } 
 
    ngOnInit(): void{
@@ -35,17 +35,52 @@ export class GetInTouchComponent implements OnInit {
 
   //Form validation
 
-  registerForm= new FormGroup ({
-    Name: new FormControl('',[Validators.required,Validators.maxLength(25)]),
-    Surname: new FormControl('',[Validators.required,Validators.maxLength(25)]),
-    Email: new FormControl('',[Validators.required,Validators.email]),
-    Comment: new FormControl('',[Validators.required,Validators.maxLength(100)])
+  form= new FormGroup ({
+    name: new FormControl('',[Validators.required,Validators.maxLength(25)]),
+    surname: new FormControl('',[Validators.required,Validators.maxLength(25)]),
+    email: new FormControl('',[Validators.required,this.customeEmailValidator]),
+    comment: new FormControl('',[Validators.required,Validators.maxLength(100)])
 
   })
 
+
+  get name (){
+    return this.form.get('name');
+  }
+
+  get surname(){
+    return this.form.get('surname');
+  }
+
+  get email(){
+    return this.form.get('email');
+  }
+
+  get comment(){
+    return this.form.get('comment');
+  }
+
+  getError(control:any) : string {
+    if(control.errors?.required && control.touched)
+      return 'Please, Filling in your email address!';
+    else if(control.errors?.emailError && control.touched)
+      return 'Please enter valid email address!';
+    else return '';
+}
+
+customeEmailValidator(control:AbstractControl){
+    const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,20}$/;
+    const value = control.value;
+    if(!pattern.test(value) && control.touched) 
+      return {
+        emailError:true
+      }
+    else return null; 
+  }
+
   getContactFormData(data:any){
       // console.warn(this.registerForm.value);
-      // console.warn(data);
+      console.warn(data);
       this.formGetInTouchService.saveData(data).subscribe((result)=>{
       console.warn(result);
       }
